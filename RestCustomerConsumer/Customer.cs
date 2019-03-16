25ac7c58b4555a4e940a534a35d3a8a2c6d27c52
basicxd <mikail-fener@hotmail.com>
2019-03-16 19:03:40 +0100

    Hey

---
+++ RestCustomerConsumer/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace RestCustomerConsumer
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Year { get; set; }
+
+        public Customer(string firstName, string lastName, int year)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Year = year;
+        }
+
+        public Customer() { }
+    }
+}
--- RestCustomerConsumer/Program.cs
+++ RestCustomerConsumer/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RestCustomerConsumer
 {
-    class Program
+    public class Program
     {
+      
+        public static async Task<IList<Customer>> GetCustomersAsync()
+        {
+            string CustomerUri = "https://localhost:44336/api/customer";
+            using (HttpClient client = new HttpClient())
+            {
+                
+                string content = await client.GetStringAsync(CustomerUri);
+                IList<Customer> cList = JsonConvert.DeserializeObject<IList<Customer>>(content);
+                return cList;
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            IList<Customer> result = GetCustomersAsync().Result;
+            Console.WriteLine(result.Count);
+            foreach (Customer C in result)
+            {
+                int ID = C.ID;
+                string firstName = C.FirstName;
+                string lastName = C.LastName;
+                int year = C.Year;
+                Console.WriteLine($"Id: {ID} First Name: {firstName} Last Name: {lastName} Year: {year}");
+
+
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
