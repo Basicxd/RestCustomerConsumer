c7d14327dc351f143ff79d1cbf3744b356cbcf98
basicxd <mikail-fener@hotmail.com>
2019-03-25 08:55:20 +0100

    MO

--- RestCustomerConsumer/Customer.cs
+++ RestCustomerConsumer/Customer.cs
@@ -1,19 +1,30 @@
-﻿namespace RestCustomerConsumer
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace RestCustomerConsumer
 {
-    public class Customer
+    public class Customer 
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Year { get; set; }
 
-        public Customer(string firstName, string lastName, int year)
+        public Customer(string firstName, string lastName, int year, int id = 0)
         {
             FirstName = firstName;
             LastName = lastName;
             Year = year;
+            ID = id;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(ID)}: {ID}, {nameof(FirstName)}: {FirstName}, {nameof(LastName)}: {LastName}, {nameof(Year)}: {Year}";
         }
 
         public Customer() { }
+
+        
     }
 }
--- RestCustomerConsumer/Program.cs
+++ RestCustomerConsumer/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -23,8 +26,60 @@ namespace RestCustomerConsumer
             }
         }
 
+        public static async Task<Customer> GetCustomersAsyncid(int id)
+        {
+            string CustomerUri = "https://localhost:44336/api/customer/" + id;
+            using (HttpClient client = new HttpClient())
+            {
+
+                string content = await client.GetStringAsync(CustomerUri);
+                Customer oneID = JsonConvert.DeserializeObject<Customer>(content);
+                
+                return oneID;
+
+
+            }
+        }
+
+        public static async Task<HttpResponseMessage> GetCustomersAsyncDelete(int id)
+        {
+            string CustomerUri = "https://localhost:44336/api/customer/" + id;
+            using (HttpClient client = new HttpClient())
+            {
+                var reponse = await client.DeleteAsync(CustomerUri);
+                return reponse;
+            }
+        }
+
+        public static async Task<HttpResponseMessage> GetCustomersAsyncAdd(string firstName, string lastName, int year)
+        {
+            string CustomerUri = "https://localhost:44336/api/customer/";
+            using (HttpClient client = new HttpClient())
+            {
+                int lastId = GetCustomersAsync().Result.Last().ID;
+
+                var jsonString = JsonConvert.SerializeObject(new Customer(firstName, lastName, year, lastId+1));
+
+
+                StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(CustomerUri, content);
+                return response;
+
+
+
+            }
+        }
+
+
+
+
         static void Main(string[] args)
         {
+            
+
+            HttpResponseMessage adding = GetCustomersAsyncAdd("Mikol", "F", 1997).Result;
+
             Console.WriteLine("Hello World!");
             IList<Customer> result = GetCustomersAsync().Result;
             Console.WriteLine(result.Count);
@@ -38,6 +93,18 @@ namespace RestCustomerConsumer
 
 
             }
+
+            //HttpResponseMessage adding = GetCustomersAsyncAdd("Mikol", "F", 1997).Result;
+       
+
+            //Customer result2 = GetCustomersAsyncid(0).Result;
+            //Console.WriteLine(result2.ToString());
+
+            //HttpResponseMessage delete = GetCustomersAsyncDelete(0).Result;
+
+            //Console.WriteLine(result);
+
+
 
             Console.ReadLine();
 
